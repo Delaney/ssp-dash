a899bdb601c0db59426bbe1e685f7bb0b6967c69
Delaney <delaneysylvans@gmail.com>
2021-11-22 00:36:44 +0100

    simple model create

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Campaign;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
 
 class CampaignController extends Controller
 {
@@ -18,12 +19,12 @@ class CampaignController extends Controller
         $validator = Validator::make(
             $request->all(),
             [
-                'name'             => 'required',
-                'date_from'     => 'required',
-                'date_to'          => 'required',
-                'total_budget'      => 'required|numeric',
-                'daily_budget'      => 'required|numeric',
-                'creatives.*'      => 'required|mimes:jpg,jpeg,png,bmp|max:20000'
+                'name'         => 'required',
+                'date_from'    => 'required|date',
+                'date_to'      => 'required|date',
+                'total_budget' => 'required|numeric',
+                'daily_budget' => 'required|numeric',
+                'creatives.*'  => 'required|mimes:jpg,jpeg,png,bmp|max:20000'
             ],
             [
                 'creatives.*.required' => 'Please upload an image',
@@ -37,5 +38,29 @@ class CampaignController extends Controller
                 'error' => $validator->errors()->first(),
                 'message' => 'invalid_input',
             ], 400);
+
+        $now = Carbon::now();
+        $date_from = Carbon::parse($request->input('date_from'));
+        $date_to = Carbon::parse($request->input('date_to'));
+
+        if ($date_from && ($date_from > $now)) {
+            return response()->json([
+                'error' => 'The start date cannot be past the current time',
+                'message' => 'invalid_input',
+            ], 400);
+        }
+
+        $campaign = Campaign::create([
+            'name'         => trim($request->input('name')),
+            'date_from'    => $date_from,
+            'date_to'      => $date_to,
+            'total_budget' => $request->input('total_budget'),
+            'daily_budget' => $request->input('daily_budget'),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'campaign' => $campaign->id
+        ]);
     }
 }
